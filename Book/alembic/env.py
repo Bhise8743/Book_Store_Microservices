14698d92b73f20d5d2d195011a499780d7c434ec
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2024-02-27 17:04:15 +0530

    [Omkar] Add : alembic migration

--- Book/alembic/env.py
+++ Book/alembic/env.py
@@ -4,11 +4,12 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-
+from Book.model import Base
+from Book.setting import postgresSQL_password,book_database_name
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
+config.set_section_option('alembic','sqlalchemy.url',f'postgresql+psycopg2://postgres:{postgresSQL_password}@localhost:5432/{book_database_name}')
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -18,7 +19,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
