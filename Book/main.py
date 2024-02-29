62324d55ca137e53552891e812e10f1acb8e41a3
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2024-02-29 11:17:07 +0530

    [Omkar] Add : added the share_book api

--- Book/main.py
+++ Book/main.py
@@ -1,25 +1,26 @@
-from fastapi import FastAPI,Security, status, Response, Depends, Path, HTTPException,Request
+from fastapi import FastAPI, Security, status, Response, Depends, Path, HTTPException, Request
 from Book.schema import BookSchema
 from fastapi.security import APIKeyHeader
 from sqlalchemy.orm import Session
 from Book.model import Book, get_db
-from Book.utils import jwt_authentication,logger
+from Book.utils import jwt_authentication, logger
 
-app = FastAPI(title='Book Store ',dependencies=[Security(APIKeyHeader(name='authorization')),Depends(jwt_authentication)])
+app = FastAPI(title='Book Store ',
+              dependencies=[Security(APIKeyHeader(name='authorization')), Depends(jwt_authentication)])
 
 
-@app.post('/add', status_code=status.HTTP_201_CREATED,tags=["Book"])
-def add_book(body: BookSchema,request:Request, response: Response, db: Session = Depends(get_db)):
+@app.post('/add', status_code=status.HTTP_201_CREATED, tags=["Book"])
+def add_book(body: BookSchema, request: Request, response: Response, db: Session = Depends(get_db)):
     try:
         print(request.state.user)
         user_data = request.state.user
         if not user_data['is_super_user']:
-            raise HTTPException(detail="You are not SuperUser",status_code=status.HTTP_400_BAD_REQUEST)
+            raise HTTPException(detail="You are not SuperUser", status_code=status.HTTP_400_BAD_REQUEST)
         if not user_data['is_verified']:
-            raise HTTPException(detail='You are not verified user',status_code=status.HTTP_400_BAD_REQUEST)
+            raise HTTPException(detail='You are not verified user', status_code=status.HTTP_400_BAD_REQUEST)
 
         data = body.model_dump()
-        data.update({'user_id':user_data['id']})
+        data.update({'user_id': user_data['id']})
         book_data = Book(**data)
         db.add(book_data)
         db.commit()
@@ -31,12 +32,12 @@ def add_book(body: BookSchema,request:Request, response: Response, db: Session =
         return {'message': str(ex), 'status': 400}
 
 
-@app.get('/{book_id}', status_code=status.HTTP_200_OK,tags=["Book"])
-def get_book_using_id(request:Request,response: Response, db: Session = Depends(get_db),
+@app.get('/get/{book_id}', status_code=status.HTTP_200_OK, tags=["Book"])
+def get_book_using_id(request: Request, response: Response, db: Session = Depends(get_db),
                       book_id=Path(..., description="Enter the book id ")):
     try:
         if not request.state.user['is_verified']:
-            raise HTTPException(detail='Not a verified user',status_code=status.HTTP_400_BAD_REQUEST)
+            raise HTTPException(detail='Not a verified user', status_code=status.HTTP_400_BAD_REQUEST)
 
         book_data = db.query(Book).filter_by(id=book_id).one_or_none()
         if book_data is None:
@@ -49,18 +50,16 @@ def get_book_using_id(request:Request,response: Response, db: Session = Depends(
 
 
 @app.get('/get', status_code=status.HTTP_200_OK, tags=["Book"])
-def get_all_books(request:Request,response: Response, db: Session = Depends(get_db)):
+def get_all_books(request: Request, response: Response, db: Session = Depends(get_db)):
     try:
         print("Ho")
         if not request.state.user['is_verified']:
-            raise HTTPException(detail='User is not verified ',status_code=status.HTTP_400_BAD_REQUEST)
+            raise HTTPException(detail='User is not verified ', status_code=status.HTTP_400_BAD_REQUEST)
         print("Hi")
         books_data = db.query(Book).all()
-        if isinstance(books_data,list):
-            print("Data is list ")
         if books_data is None:
             raise HTTPException(detail='User Not Added any book', status_code=status.HTTP_400_BAD_REQUEST)
-
+        print(books_data)
         return {'message': 'Books Found', 'status': 200, 'data': books_data}
     except Exception as ex:
         logger.exception(ex)
@@ -68,14 +67,14 @@ def get_all_books(request:Request,response: Response, db: Session = Depends(get_
         return {'message': str(ex), 'status': 400}
 
 
-@app.put('/update/{book_id}', status_code=status.HTTP_200_OK,tags=["Book"])
-def update_book(body: BookSchema,request:Request, response: Response, db: Session = Depends(get_db),
+@app.put('/update/{book_id}', status_code=status.HTTP_200_OK, tags=["Book"])
+def update_book(body: BookSchema, request: Request, response: Response, db: Session = Depends(get_db),
                 book_id=Path(..., description="Enter the book id ")):
     try:
         if not request.state.user['is_verified']:
-            raise HTTPException(detail='User is Not verified ',status_code=status.HTTP_400_BAD_REQUEST)
+            raise HTTPException(detail='User is Not verified ', status_code=status.HTTP_400_BAD_REQUEST)
         if not request.state.user['is_super_user']:
-            raise HTTPException(detail='You are not super user',status_code=status.HTTP_400_BAD_REQUEST)
+            raise HTTPException(detail='You are not super user', status_code=status.HTTP_400_BAD_REQUEST)
 
         book_data = db.query(Book).filter_by(id=book_id).one_or_none()
         if book_data is None:
@@ -90,13 +89,14 @@ def update_book(body: BookSchema,request:Request, response: Response, db: Sessio
         return {'message': str(ex), 'status': 400}
 
 
-@app.delete('/del/{book_id}', status_code=status.HTTP_200_OK,tags=["Book"])
-def delete_book(request:Request,response: Response, db: Session = Depends(get_db), book_id=Path(..., description="Enter the book id")):
+@app.delete('/del/{book_id}', status_code=status.HTTP_200_OK, tags=["Book"])
+def delete_book(request: Request, response: Response, db: Session = Depends(get_db),
+                book_id=Path(..., description="Enter the book id")):
     try:
         if not request.state.user['is_verified']:
-            raise HTTPException(detail='You are not verified user',status_code=status.HTTP_400_BAD_REQUEST)
+            raise HTTPException(detail='You are not verified user', status_code=status.HTTP_400_BAD_REQUEST)
         if not request.state.user['is_super_user']:
-            raise HTTPException(detail='You are not Super user',status_code=status.HTTP_400_BAD_REQUEST)
+            raise HTTPException(detail='You are not Super user', status_code=status.HTTP_400_BAD_REQUEST)
 
         book_data = db.query(Book).filter_by(id=book_id).one_or_none()
         if book_data is None:
@@ -106,5 +106,18 @@ def delete_book(request:Request,response: Response, db: Session = Depends(get_db
         return {'message': "Book Deleted Successfully", 'status': 200}
     except Exception as ex:
         logger.exception(ex)
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {'message': str(ex), 'status': 400}
+
+
+@app.get('/share_book/{book_id}', status_code=status.HTTP_200_OK)
+def share_book(book_id: int, response: Response, db: Session = Depends(get_db)):
+    try:
+        book_data = db.query(Book).filter_by(id=book_id).one_or_none()
+        if book_data is None:
+            raise HTTPException(detail='Book not found', status_code=status.HTTP_404_NOT_FOUND)
+        print("Hi I am in the share book ")
+        return {'message': "Book Found successful", 'status': 200, 'book_data': book_data}
+    except Exception as ex:
         response.status_code = status.HTTP_400_BAD_REQUEST
         return {'message': str(ex), 'status': 400}
